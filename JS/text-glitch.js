25c99de04d3e294fa3e751e0d766c59897a58ba3
Quixxo <ngzeie8161@gmail.com>
2024-06-02 10:03:41 +0700

    text glitch fix

--- JS/text-glitch.js
+++ JS/text-glitch.js
@@ -1,36 +1,71 @@
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+// let interval = null;
+// let animation = false;
+
+
+// // .nav-item là tên element để áp dụng cái effect, tùng code tới đoạn nav thì tự đổi tên rồi áp vào nhé
+// document.querySelectorAll(".nav_item").forEach(element => {
+//     element.addEventListener('mouseover', event => {
+//         if(animation){
+//             return;
+//         }
+//         animation = true;
+//         let iteration = 0;
+//         clearInterval(interval);
+
+//         interval = setInterval(() => {
+//             event.target.innerText = event.target.innerText
+//             .split("")
+//             .map((letter, index) => {
+//                 if (index < iteration) {
+//                 return event.target.dataset.value[index];
+//                 }
+//                 return letters[Math.floor(Math.random() * 26)];
+//             })
+//             .join("");
+
+//             if (iteration >= event.target.dataset.value.length) {
+//                 clearInterval(interval);
+//                 animation = false
+//             }
+
+//             iteration += 1 / 2;
+//         }, 30);
+//         });
+//     });
+
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-let interval = null;
-let animation = false;
+    document.querySelectorAll(".glitch_item").forEach(element => {
+        let interval = null;
+        let animation = false;
 
-
-// .nav-item là tên element để áp dụng cái effect, tùng code tới đoạn nav thì tự đổi tên rồi áp vào nhé
-document.querySelectorAll(".nav_item").forEach(element => {
-    element.addEventListener('mouseover', event => {
-        if(animation){
-            return;
-        }
-        animation = true;
-        let iteration = 0;
-        clearInterval(interval);
-
-        interval = setInterval(() => {
-            event.target.innerText = event.target.innerText
-            .split("")
-            .map((letter, index) => {
-                if (index < iteration) {
-                return event.target.dataset.value[index];
-                }
-                return letters[Math.floor(Math.random() * 26)];
-            })
-            .join("");
-
-            if (iteration >= event.target.dataset.value.length) {
-                clearInterval(interval);
-                animation = false
+        element.addEventListener('mouseover', event => {
+            if (animation) {
+                return;
             }
+            animation = true;
+            let iteration = 0;
+            clearInterval(interval);
 
-            iteration += 1 / 2;
-        }, 30);
+            interval = setInterval(() => {
+                event.target.innerText = event.target.innerText
+                    .split("")
+                    .map((letter, index) => {
+                        if (index < iteration) {
+                            return event.target.dataset.value[index];
+                        }
+                        return letters[Math.floor(Math.random() * 26)];
+                    })
+                    .join("");
+
+                if (iteration >= event.target.dataset.value.length) {
+                    clearInterval(interval);
+                    animation = false;
+                }
+
+                iteration += 1 / 2;
+            }, 30);
         });
     });

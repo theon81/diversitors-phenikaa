5c92145f99a5adea5cff3ee8c8aff1d646037af6
kieutung <kieuthanhtung0502@gmail.com>
2024-06-02 07:59:03 +0700

    nothing

--- JS/text-glitch.js
+++ JS/text-glitch.js
@@ -1,36 +1,35 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-let interval = null;
-let animation = false;
+// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 
 // .nav-item là tên element để áp dụng cái effect, tùng code tới đoạn nav thì tự đổi tên rồi áp vào nhé
-document.querySelectorAll(".nav_item").forEach(element => {
-    element.addEventListener('mouseover', event => {
-        if(animation){
-            return;
-        }
-        animation = true;
-        let iteration = 0;
-        clearInterval(interval);
+// document.querySelectorAll(".text_glitch").forEach(element => {
+//     let interval = null;
+//     let animation = false;
+//     element.addEventListener('mouseover', event => {
+//         if(animation){
+//             return;
+//         }
+//         animation = true;
+//         let iteration = 0;
+//         clearInterval(interval);
 
-        interval = setInterval(() => {
-            event.target.innerText = event.target.innerText
-            .split("")
-            .map((letter, index) => {
-                if (index < iteration) {
-                return event.target.dataset.value[index];
-                }
-                return letters[Math.floor(Math.random() * 26)];
-            })
-            .join("");
+//         interval = setInterval(() => {
+//             event.target.innerText = event.target.innerText
+//             .split("")
+//             .map((letter, index) => {
+//                 if (index < iteration) {
+//                 return event.target.dataset.value[index];
+//                 }
+//                 return letters[Math.floor(Math.random() * 26)];
+//             })
+//             .join("");
 
-            if (iteration >= event.target.dataset.value.length) {
-                clearInterval(interval);
-                animation = false
-            }
+//             if (iteration >= event.target.dataset.value.length) {
+//                 clearInterval(interval);
+//                 animation = false
+//             }
 
-            iteration += 1 / 2;
-        }, 30);
-        });
-    });
+//             iteration += 1 / 2;
+//         }, 30);
+//         });
+//     });

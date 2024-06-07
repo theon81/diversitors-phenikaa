db5fe2ca2965f287c79dd7b10810f62285418fa5
kieutung <kieuthanhtung0502@gmail.com>
2024-06-07 17:09:08 +0700

    modify text-glitch

--- JS/text-glitch.js
+++ JS/text-glitch.js
@@ -1,41 +1,67 @@
-
 const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    document.querySelectorAll(".glitch_item").forEach(element => {
-        let interval = null;
-        let animation = false;
+document.querySelectorAll(".glitch_item").forEach(element => {
+    let interval = null;
+    let animation = false;
 
-        const glitchText = (event) => {
-            if (animation) {
-                return;
-            }
-            animation = true;
-            let iteration = 0;
-            clearInterval(interval);
-            interval = setInterval(() => {
-                event.target.innerText = event.target.innerText
-                    .split("")
-                    .map((letter, index) => {
-                        if (index < iteration) {
-                            return event.target.dataset.value[index];
-                        }
-                        return letters[Math.floor(Math.random() * 26)];
-                    })
-                    .join("");
-    
-                if (iteration >= event.target.dataset.value.length) {
-                    clearInterval(interval);
-                    animation = false;
-                }
-    
-                iteration += 1 / 2;
-            },30);
+    const autoAppear = element.dataset.autoAppear === "true";
+    const appearOnce = element.dataset.appearOnce === "true";
+    const delay = parseInt(element.dataset.delay, 10) || 0;
+    const animationSpeed = parseFloat(element.dataset.animationSpeed) || 1;
+    const writeInOnHover = element.dataset.writeInOnHover === "true";
+    let externalHovering = false;
+    let appeared = 0;
+
+    const glitchText = (event) => {
+        if (animation) {
+            return;
         }
+        animation = true;
+        let iteration = 0;
+        clearInterval(interval);
+        interval = setInterval(() => {
+            event.target.innerText = event.target.innerText
+                .split("")
+                .map((letter, index) => {
+                    if (index < iteration) {
+                        return event.target.dataset.value[index];
+                    }
+                    return letters[Math.floor(Math.random() * letters.length)];
+                })
+                .join("");
 
-        element.addEventListener('mouseover', event =>{
-            glitchText(event);
-        });
+            if (iteration >= event.target.dataset.value.length) {
+                clearInterval(interval);
+                animation = false;
+            }
 
-        
+            iteration += (1 / 2) * animationSpeed;
+        }, 30 / animationSpeed);
     }
-);
+
+    const handleMouseOver = (event) => {
+        if (externalHovering || writeInOnHover) {
+            glitchText(event);
+        }
+    }
+
+    element.addEventListener('mouseover', handleMouseOver);
+
+    if (autoAppear) {
+        const handleScroll = () => {
+            const rect = element.getBoundingClientRect();
+            if (rect.top < window.innerHeight && rect.bottom > 0) {
+                if (appearOnce && appeared > 0) {
+                    return;
+                }
+                setTimeout(() => {
+                    glitchText({ target: element });
+                    appeared++;
+                }, delay);
+                window.removeEventListener('scroll', handleScroll);
+            }
+        }
+        window.addEventListener('scroll', handleScroll);
+        handleScroll();
+    }
+});

ca822c874342d607a01ef2fce1e3dfd6b0b5e9bf
kieutung <kieuthanhtung0502@gmail.com>
2024-06-08 00:54:08 +0700

    sidebar

--- JS/sidebar-show.js
+++ JS/sidebar-show.js
@@ -2,14 +2,26 @@ document.addEventListener('DOMContentLoaded',() => {
     let container = document.getElementById('menu');
     let sidebarContainer = document.getElementById('sidebar_container');
     let element = document.getElementById('sidebar_box');
+    let close = document.getElementById('sb-close-button');
     let isClick = false;
 
-    container.addEventListener('click',() =>{
+    container.addEventListener('click',(event) =>{
+        event.stopPropagation();
         if(!isClick){
             sidebarContainer.classList.add('show');
             element.classList.add('show');
             isClick = true;
         } else {
+            sidebarContainer.classList.remove('show');
+            element.classList.remove('show');
+            isClick = false;
+        }
+    });
+    element.addEventListener('click',(event) =>{
+        event.stopPropagation();
+    });
+    close.addEventListener('click',() =>{
+        if(isClick){
             sidebarContainer.classList.remove('show');
             element.classList.remove('show');
             isClick = false;
